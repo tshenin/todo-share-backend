dd5726d7482d0764dcd46ccd666b44bb01b2fc63
Tim Shenin <t.shenin@adguard.com>
2019-02-21 20:31:26 +0300

    Put routes separated

---
+++ routes/basic.js
@@ -0,0 +1,7 @@
+const Router = require('koa-router');
+const meta = require('../package.json');
+const router = new Router();
+
+router.get('/', ctx => ctx.body = meta.version);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 require('@babel/register');
 
 const Koa = require('koa');
-const Router = require('koa-router');
-const meta = require('./package.json');
+const router = require('./routes/basic');
 const app = new Koa();
-const router = new Router();
 const PORT = 3000;
-
-router.get('/', ctx => ctx.body = meta.version);
 
 app.use(router.routes());
 app.use(router.allowedMethods());
