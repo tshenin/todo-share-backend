8df4008c6d606aaa8ba4bfcf463dab0e80b7b392
Tim Shenin <shenin.timofei@yandex.ru>
2019-03-11 22:39:22 +0300

    Some seed for boards

--- src/db/seeds/board_seed.js
+++ src/db/seeds/board_seed.js
@@ -1,33 +1,17 @@
 
-exports.seed = async (knex, Promise) => {
-    // Deletes ALL existing entries
-    const todos = await knex('todos').select('*');
+exports.seed = async knex => {
 
     await knex('boards').del();
 
-    todos.forEach((item, index) => {
-        // todo 
+    await knex('boards').insert({
+        title: `board 1`,
+        desc: `Description 1`,
     });
 
     await knex('boards').insert({
-        title: 'board 1',
-        desc: 'Description one',
-        todos: [1]
+        title: `board 2`,
+        desc: `Description 3`,
     });
-    await knex('boards').insert({
-        title: 'board 2',
-        desc: 'Description two',
-        todos: [2]
-    })
-    await knex('boards').insert({
-        title: 'board 3',
-        desc: 'Description three',
-        todos: [3]
-    })
-    await knex('boards').insert({
-        title: 'board 4',
-        desc: 'Description four',
-        todos: [4, 5]
-    })
+
     return knex;
 };
