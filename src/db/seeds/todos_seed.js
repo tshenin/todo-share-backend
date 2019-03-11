2d7edf2b1c67b0772b6414c5ab69bbbef8cc9d69
Tim Shenin <shenin.timofei@yandex.ru>
2019-03-12 00:00:29 +0300

    Update seeds

--- src/db/seeds/todos_seed.js
+++ src/db/seeds/todos_seed.js
@@ -3,20 +3,28 @@ exports.seed = async (knex, Promise) => {
     // Deletes ALL existing entries
     await knex('todos').del();
     await knex('todos').insert({
+        id: 1,
         title: 'Todo 1',
-        desc: 'Description one'
+        desc: 'Description one',
+        board_id: 1,
     });
     await knex('todos').insert({
+        id: 2,
         title: 'Todo 2',
-        desc: 'Description two'
+        desc: 'Description two',
+        board_id: 1,
     })
     await knex('todos').insert({
+        id: 3,
         title: 'Todo 3',
-        desc: 'Description three'
+        desc: 'Description three',
+        board_id: 2,
     })
     await knex('todos').insert({
+        id: 4,
         title: 'Todo 4',
-        desc: 'Description four'
+        desc: 'Description four',
+        board_id: 2,
     })
     return knex;
 };
